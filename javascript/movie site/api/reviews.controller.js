c3d522425da7fc94b6e4f263cfeb3148fecc7ca5
Oğuzhan Hastoprak <160526539+OguzhanHastoprak@users.noreply.github.com>
2024-11-24 04:55:40 +0300

    js

---
+++ javascript/movie site/api/reviews.controller.js
@@ -0,0 +1,20 @@
+import ReviewsDAO from "../dao/reviewsDAO.js"
+
+export default class ReviewsController {
+    static async apiPostReview(req, res, next){
+        try {
+            const movieId = req.body.movieId
+            const review = req.body.review
+            const user = req.body.user
+
+            const reviewResponse = await ReviewsDAO.addReview(
+                movieId,
+                user,
+                review
+            )
+            res.json({status: "success"})
+        } catch (e) {
+            res.status(500).json({error: e.message})
+        }
+    }
+}
